371e3a926489ceb442fa489355f38ce7d4ef26bc
daisyisibor <daisyisibor8@gmail.com>
2024-03-24 16:06:27 -0400

    added more values on employee, role and department table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,45 @@
+INSERT INTO department (name) VALUES 
+    ('Engineering'),
+    ('Sales'),
+    ('Finance'),
+    ('IT'),
+    ('Customer Care'),
+    ('Marketing'),
+    ('HR'),
+    ('Lawyer');
 
-INSERT INTO department (name) VALUES ('Engineering'), ('Sales'), ('Finance');
-
-INSERT INTO role (department_id,title, salary) VALUES 
-    (1,'Software Engineer', 80000,),
-    (2,'Sales Manager', 60000,),
-    (3,'Accountant', 50000,),
+INSERT INTO role (department_id, title, salary) VALUES 
+    (1, 'Design Engineer', 105000),
+    (2, 'Sales Manager', 60000),
+    (3, 'Accountant', 80000),
+    (4, 'Full Stack Developer (fsd)', 140000),
+    (5, 'Cyber Security', 110000),
+    (6, 'Customer Care Agent', 54000),
+    (7, 'Customer Care Manger', 65000),
+    (8, 'Scrum Master', 120000),
+    (9, 'HR', 70000),
+    (10, 'Marketing', 68000),
+    (11, 'Sales Agent', 67000),
+    (12, 'Lawyer',200000);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-    ('John', 'Doe', 1, NULL),
-    ('Jane', 'Smith', 2, 1),
-    ('Michael', 'Johnson', 3, NULL);
-
+    ('Keth', 'Kennedy', 1, NULL),
+    ('Andrea', 'Smith', 2, NULL),
+    ('Elizabeth', 'Johnson', 11, 2),
+    ('Tanya', 'Lovely', 6, 7),
+    ('Adams', 'Jacob', 7, NULL),
+    ('Taylor', 'Freeman', 6, 7),
+    ('Dean', 'Winchester', 5, 8),
+    ('Damon', 'Salvator', 8, NULL ),
+    ('Olivia', 'pope', 12, NULL),
+    ('Melissa', 'Grant', 9, NULL),
+    ('Yang', 'Liu', 6, 7),
+    ('Lee', 'Min-ho', 3, 9),
+    ('Natasha', 'James', 9, NULL),
+    ('Osas', 'Isibor', 4, 8),
+    ('Peter', 'daddeta', 5, 8),
+    ('Grace', 'lukeman', 10, 9),
+    ('veronica', 'Stein', 11, 2),
+    ('Amira', 'Muhammad', 6, 7),
+    ('Luke', 'Gomez', 4, 8),
+    ('Tom', 'Timothy', 11, 2);
