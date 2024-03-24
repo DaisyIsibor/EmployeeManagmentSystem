7221bd445c7a6bc721e3137c79ecf2470dcc5faf
daisyisibor <daisyisibor8@gmail.com>
2024-03-24 17:00:59 -0400

    still working on proper fuctionality on function main menu

--- index.js
+++ index.js
@@ -9,31 +9,49 @@ function mainMenu() {
 +-------------------------------------------------+
 `);
 
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What would you like to do?',
-            choices: [
-                'View Departments',
-                'View Roles',
-                'View Employees',
-                'View employees by Manager',
-                'Add Department',
-                'Add Role',
-                'Add Employee',
-                'Update Employee Role',
-                'Update employee managers',
-                'Delete an employee',
-                'Delete departments',
-                'Delete a role',
-                'Exit'
-            ]
-        } // This closing curly brace was missing in your code
-    ]).then(answer => {
-        // Handle user's selection here
-        console.log('You selected:', answer.action);
-    });
+    // Function to display the menu
+    function displayMenu() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View Departments',
+                    'View Roles',
+                    'View Employees',
+                    'View Employees by Manager',
+                    'Add Department',
+                    'Add Role',
+                    'Add Employee',
+                    'Update Employee Role',
+                    'Update Employee Manager',
+                    'Delete an Employee',
+                    'Delete Department',
+                    'Delete a Role',
+                    'Exit'
+                ]
+            }
+        ]).then(answer => {
+            // Handle user's selection here
+            console.log('You selected:', answer.action);
+            if (answer.action === 'Exit') {
+                // If the user chooses "Exit", exit the program
+                console.log('Exiting...');
+            } else {
+                // If the user chooses other options, display the menu again
+                displayMenu();
+            }
+        }).catch(error => {
+            console.error('Error occurred:', error);
+        });
+    }
+
+    // Call the function to display the menu initially
+    displayMenu();
 }
 
 module.exports = mainMenu;
+
+// Call the mainMenu function to start the interaction
+mainMenu();
